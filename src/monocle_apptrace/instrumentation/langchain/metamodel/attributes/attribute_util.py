e431d3c537e3f927352ef4831fb5f06560394fbf
Ravi Anne <ravi@okahu.ai>
2024-12-16 20:04:32 +0530

    updated attribute_util.py to handle input/output
    Signed-off-by: Ravi Anne <ravi@okahu.ai>

--- src/monocle_apptrace/instrumentation/langchain/metamodel/attributes/attribute_util.py
+++ src/monocle_apptrace/instrumentation/langchain/metamodel/attributes/attribute_util.py
@@ -6,11 +6,7 @@ and assistant messages from various input formats.
 import logging
 from urllib.parse import urlparse
 
-from monocle_apptrace.instrumentation.common.utils import (
-    Option,
-    get_attribute,
-    try_option,
-)
+from monocle_apptrace.instrumentation.common.utils import Option, try_option
 
 DATA_INPUT_KEY = "data.input"
 
@@ -18,80 +14,40 @@ logger = logging.getLogger(__name__)
 def extract_messages(args):
     """Extract system and user messages"""
     try:
-        system_message, user_message = "", ""
-        args_input = get_attribute(DATA_INPUT_KEY)
-        if args_input:
-            user_message = args_input
-            return system_message, user_message
+        messages = []
         if args and isinstance(args, tuple) and len(args) > 0:
             if hasattr(args[0], "messages") and isinstance(args[0].messages, list):
                 for msg in args[0].messages:
                     if hasattr(msg, 'content') and hasattr(msg, 'type'):
-                        if msg.type == "system":
-                            system_message = msg.content
-                        elif msg.type in ["user", "human"]:
-                            user_message = msg.content
-            elif isinstance(args[0], list):
-                for msg in args[0]:
-                    if hasattr(msg, 'content') and hasattr(msg, 'role'):
-                        if msg.role == "system":
-                            system_message = msg.content
-                        elif msg.role in ["user", "human"]:
-                            user_message = extract_query_from_content(msg.content)
-        return system_message, user_message
+                        messages.append({msg.type: msg.content})
+        return [str(d) for d in messages]
     except Exception as e:
         logger.warning("Warning: Error occurred in extract_messages: %s", str(e))
-        return "", ""
+        return []
 
 
 def extract_assistant_message(response):
     try:
         if isinstance(response, str):
-            return response
+            return [response]
         if hasattr(response, "content"):
-            return response.content
+            return [response.content]
         if hasattr(response, "message") and hasattr(response.message, "content"):
-            return response.message.content
-        if "replies" in response:
-            if hasattr(response['replies'][0], 'content'):
-                return response['replies'][0].content
-            else:
-                return response['replies'][0]
-        return ""
+            return [response.message.content]
     except Exception as e:
         logger.warning("Warning: Error occurred in extract_assistant_message: %s", str(e))
-        return ""
-
-
-def extract_query_from_content(content):
-    try:
-        query_prefix = "Query:"
-        answer_prefix = "Answer:"
-        query_start = content.find(query_prefix)
-        if query_start == -1:
-            return None
-
-        query_start += len(query_prefix)
-        answer_start = content.find(answer_prefix, query_start)
-        if answer_start == -1:
-            query = content[query_start:].strip()
-        else:
-            query = content[query_start:answer_start].strip()
-        return query
-    except Exception as e:
-        logger.warning("Warning: Error occurred in extract_query_from_content: %s", str(e))
-        return ""
-
+        return []
 
 
 def extract_provider_name(instance):
     provider_url: Option[str] = try_option(getattr, instance.client._client.base_url, 'host')
     if provider_url.is_none():
         provider_url = try_option(getattr, instance, 'api_base').and_then(lambda url: urlparse(url).hostname)
-    
+
     return provider_url.unwrap_or(None)
 
-# to be validated for non-langchain use cases
+
+# to be validated for non-langchain
 def extract_inference_endpoint(instance):
     inference_endpoint: Option[str] = try_option(getattr, instance.client._client, 'base_url').map(str)
     if inference_endpoint.is_none():
--- src/monocle_apptrace/instrumentation/langchain/metamodel/attributes/config/inference.py
+++ src/monocle_apptrace/instrumentation/langchain/metamodel/attributes/config/inference.py
@@ -6,51 +6,46 @@ from monocle_apptrace.instrumentation.langchain.metamodel.attributes import (
 inference ={
   "type": "inference",
   "attributes": [
-    # [
-    #   {
-    #     "_comment": "provider type ,name , deployment , inference_endpoint",
-    #     "attribute": "type",
-    #     "accessor": lambda arguments:'inference.azure_oai'
-    #   },
-    #   {
-    #     "attribute": "provider_name",
-    #     "accessor": lambda arguments:attribute_util.extract_provider_name(arguments['instance'])
-    #   },
-    #   {
-    #     "attribute": "deployment",
-    #     "accessor": lambda arguments: resolve_from_alias(arguments['instance'].__dict__, ['engine', 'azure_deployment', 'deployment_name', 'deployment_id', 'deployment'])
-    #   },
-    #   {
-    #     "attribute": "inference_endpoint",
-    #     "accessor": lambda arguments: resolve_from_alias(arguments['instance'].__dict__, ['azure_endpoint', 'api_base']) or attribute_util.extract_inference_endpoint(arguments['instance'])
-    #   }
-    # ],
-    # [
-    #   {
-    #     "_comment": "LLM Model",
-    #     "attribute": "name",
-    #     "accessor": lambda arguments: resolve_from_alias(arguments['instance'].__dict__, ['model', 'model_name'])
-    #   },
-    #   {
-    #     "attribute": "type",
-    #     "accessor": lambda arguments: 'model.llm.'+resolve_from_alias(arguments['instance'].__dict__, ['model', 'model_name'])
-    #   }
-    # ]
+    [
+      {
+        "_comment": "provider type ,name , deployment , inference_endpoint",
+        "attribute": "type",
+        "accessor": lambda arguments:'inference.azure_oai'
+      },
+      {
+        "attribute": "provider_name",
+        "accessor": lambda arguments:attribute_util.extract_provider_name(arguments['instance'])
+      },
+      {
+        "attribute": "deployment",
+        "accessor": lambda arguments: resolve_from_alias(arguments['instance'].__dict__, ['engine', 'azure_deployment', 'deployment_name', 'deployment_id', 'deployment'])
+      },
+      {
+        "attribute": "inference_endpoint",
+        "accessor": lambda arguments: resolve_from_alias(arguments['instance'].__dict__, ['azure_endpoint', 'api_base']) or attribute_util.extract_inference_endpoint(arguments['instance'])
+      }
+    ],
+    [
+      {
+        "_comment": "LLM Model",
+        "attribute": "name",
+        "accessor": lambda arguments: resolve_from_alias(arguments['instance'].__dict__, ['model', 'model_name'])
+      },
+      {
+        "attribute": "type",
+        "accessor": lambda arguments: 'model.llm.'+resolve_from_alias(arguments['instance'].__dict__, ['model', 'model_name'])
+      }
+    ]
   ],
   "events": [
     { "name":"data.input",
       "attributes": [
 
-        {
-            "_comment": "this is instruction to LLM",
-            "attribute": "system",
-            "accessor": lambda arguments: attribute_util.extract_messages(arguments)[0]
-        },
-        {
-            "_comment": "this is user query to LLM",
-            "attribute": "user",
-            "accessor": lambda arguments: attribute_util.extract_messages(arguments)[1]
-        }
+          {
+              "_comment": "this is instruction and user query to LLM",
+              "attribute": "input",
+              "accessor": lambda arguments: attribute_util.extract_messages(arguments['args'])
+          }
       ]
     },
     {
